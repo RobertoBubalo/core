37025f09c6bb7aa5de6bee22ff75fa32f52b382e
Roberto <roberto.bubalo@zutec.com>
2020-03-04 15:16:25 +0000

    initial commit

---
+++ Core/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Core
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            CreateWebHostBuilder (args).Build ().Run ();
+        }
+
+        // Setting up the web server
+        // Loading the host and application configuration from various configuration sources
+        // Configures logging
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+                .UseStartup<Startup> ();
+    }
+    // An asp.Net core app can be hosted InProcess || OutOfProcess
+}
