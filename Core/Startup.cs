135c788803421193f7ddb17d103e51c76a06214f
Roberto <roberto.bubalo@zutec.com>
2020-03-04 16:53:14 +0000

    some basics implemented while testing...

--- Core/Startup.cs
+++ Core/Startup.cs
@@ -5,30 +5,59 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Core
 {
     public class Startup
     {
+        private IConfiguration _config;
+        private IServiceCollection _services;
+
+        public Startup (IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices (IServiceCollection services)
         {
+            _services = services;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
+            // Middleware -> Handles http request or response
+            // Short circuiting the middleware
+
             if (env.IsDevelopment ())
             {
                 app.UseDeveloperExceptionPage ();
             }
 
+            app.UseStaticFiles ();
+
+            app.Use (async (context, next) =>
+            {
+                logger.LogInformation ("mw1: Incoming request");
+                await next ();
+                logger.LogInformation ("mw1: Outgoing response");
+            });
+
+            app.Use (async (context, next) =>
+            {
+                logger.LogInformation ("mw2: Incoming request");
+                await next ();
+                logger.LogInformation ("mw2: Outgoing response");
+            });
+
             app.Run (async (context) =>
-             {
-                 await context.Response.WriteAsync ("Hello World!");
-             });
+            {
+                await context.Response.WriteAsync ("Hello from second middleware!");
+            });
         }
     }
 }
