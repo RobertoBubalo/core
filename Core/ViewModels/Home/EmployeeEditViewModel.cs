71ccb429977738cd78d192caf16d3750661d7457
RobertoBubalo1 <robertobubalo@gmail.com>
2020-03-12 07:45:00 +0000

    missed some files in last commit

---
+++ Core/ViewModels/Home/EmployeeEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels.Home
+{
+    public class EmployeeEditViewModel : EmployeeCreateViewModel
+    {
+        public int Id { get; set; }
+        public string ExistingImagePath { get; set; }
+    }
+}
